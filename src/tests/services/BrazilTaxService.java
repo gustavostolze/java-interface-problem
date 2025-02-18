aa6107de3ff7ce4841fa2ca28da9e2f0d0f51e33
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2025-02-18 17:38:00 -0300

    initial commit

---
+++ src/tests/services/BrazilTaxService.java
@@ -0,0 +1,12 @@
+package tests.services;
+
+public class BrazilTaxService {
+	
+	public Double tax(Double amount) {
+		if (amount <= 100.0) {
+			return amount * 0.2;
+		} else {
+			return amount * 0.15;
+		}
+	}
+}
